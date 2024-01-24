24aec0b24659376b440160d83dc191530114948b
ragmer09toske <retsepile.raymondshao@gmail.com>
2024-01-24 05:03:14 -0700

    Timeframes

---
+++ src/pages/LandingPages/Dms/Timeframe.js
@@ -0,0 +1,15 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+export const Timeframe = () => {
+  return (
+    <Box>
+        <Box sx={{
+            minWidth: "100vw",
+            height: 2,
+            color: "gray"
+
+        }}></Box>
+    </Box>
+  )
+}
